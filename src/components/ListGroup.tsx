cd80e34c58298a47fa6a62f85bbe6f6055ce1f49
Sharmavatsal <sharmavatsal144@gmail.com>
2026-08-31 15:37:45 +0530

    printing the message dynamically (i.e No items found)

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,20 +2,21 @@ import { Fragment } from "react";
 // here we are using Fragment to avoid adding extra nodes to the DOM. It is a common practice in React to use Fragment when you want to return multiple elements without adding an extra div or other container element.
 // in the return statement we are using <> and </> which is a shorthand for <Fragment> and </Fragment>. It is a common practice in React to use this shorthand when you want to return multiple elements without adding an extra div or other container element.
 function ListGroup() {
-    const items = [
+    let items = [
         'New York',
         'San Francisco',
         'Tokyo',
         'London',
         'Paris'
     ];
+    items = [];
 
     
 
     return (
         <> 
         <h1>List</h1>
-
+        {items.length === 0 && <p>No items found</p> }   
     <ul className="list-group">
     {items.map(item => (
         <li key={item}>{item}</li>
